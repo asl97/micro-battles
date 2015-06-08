b03a6a61eabe951635285c69c4be6a664c0a0aac
PChicken <tardisbluedoctorwho1@gmail.com>
2015-06-07 21:19:04 -0400

    Made init.lua
    First real code!
    Note to self:
    add pvp and match joining

---
+++ init.lua
@@ -0,0 +1,19 @@
+minetest.register_privilege(
+	"micromine",
+	{
+		description = "Allows you to mine micro battle blocks. Must use  /grant <name> micromine to use.",
+		give_to_singleplayer = false
+	})
+minetest.register_node(
+	"mbBlock",
+	{
+		diggable = true
+		can_dig = function(pos, player)
+			if minetest.check_player_privs(player, {micromine}) then
+				return true
+			else
+				return false
+			end
+		end
+		walkable = true
+	})
